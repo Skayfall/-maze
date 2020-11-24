d5ae92b1a0b7d073e94181ea633409e704bbfa3c
Skayfall <58567973+Skayfall@users.noreply.github.com>
2020-11-24 18:08:56 +0300

    Update field.py

--- field.py
+++ field.py
@@ -1,1 +1,15 @@
+class LabirintTurtle:
+    def __init__(self):
+        self.row = 0
+        self.col = 0
+        self.turtle = False
 
+    def load_map(self, name):
+        self.field = open(name, 'r')
+        self.line = self.field.read().split('\n')
+        self.row = self.line[-2]
+        self.col = self.line[-1]
+        
+    def show_map(self):
+        for i in range(len(self.line) - 2):
+             print(self.line[i])
