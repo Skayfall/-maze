b8db1df9369d9cf7c2458f385612fb4184fff988
Skayfall <58567973+Skayfall@users.noreply.github.com>
2020-11-26 16:18:49 +0300

    Update LabirintTurtle.py

--- LabirintTurtle.py
+++ LabirintTurtle.py
@@ -13,7 +13,9 @@ class LabirintTurtle:
 
     def show_map(self, turtle=False):
         for i in range(len(self.line) - 2):
-            if turtle:
-                f = self.line[self.row]
-                self.line[self.row] = f[:self.col] + 'A' + f[self.col + 1::]
+            if turtle and self.line[self.row][self.col] != '*':
+                if 0 < self.row < len(self.line[0]) - 1:
+                    if 0 < self.col < len(self.line[0]) - 1:
+                        f = self.line[self.row]
+                        self.line[self.row] = f[:self.col] + 'A' + f[self.col + 1::]
             print(colored(self.line[i], 'magenta'))
